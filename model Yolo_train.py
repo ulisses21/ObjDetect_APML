9cfae82e2ca402b51c55303613753041288cbc23
ulisses21 <ulissesquaresma@hotmail.com>
2023-11-25 15:57:50 +0100

    Delete model Yolo_train.py

--- model Yolo_train.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO('yolov8n.pt') # pre-trained model
-train_results = model.train(data="custom_dataset.yaml", epochs=100, imgsz=320, device=[0,1])#train with dataset from OpenImagesV7 only person class
-model.val()
-
-
